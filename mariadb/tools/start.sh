48f5c44871baf9bcc94329c433460ac74a15856a
pboidin <pierrelouisboidin@gmail.com>
2023-02-18 13:35:58 +0100

    add modification

--- mariadb/tools/start.sh
+++ mariadb/tools/start.sh
@@ -4,10 +4,10 @@ set -vxe
 if [ ! -f "$SETFILE" ]; then
     service mysql start
     echo "CREATE DATABASE IF NOT EXISTS \`${NAME_DB}\`;" | mysql
-    echo "CREATE USER IF NOT EXISTS \`${USER_DB}\`@'localhost' IDENTIFIED BY '${PASSWORD_DB}';" | mysql
+    echo "CREATE USER IF NOT EXISTS \`${USER_DB}\`@'%' IDENTIFIED BY '${PASSWORD_DB}';" | mysql
     echo "GRANT ALL PRIVILEGES ON \`${NAME_DB}\`.* TO \`${USER_DB}\`@'%' IDENTIFIED BY '${PASSWORD_DB}';" | mysql
-    echo "FLUSH PRIVILEGES;" | mysql
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD_DB}';" | mysql
+    echo "FLUSH PRIVILEGES;" | mysql
     touch $SETFILE
     service mysql stop 
 fi
